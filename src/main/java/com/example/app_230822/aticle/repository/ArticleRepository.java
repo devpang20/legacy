ec589f622286b397e14985a94c2d09965136c5ea
sik2 <devpang20@gmail.com>
2023-08-22 12:30:01 +0900

    feat: Article Insert, getArticleById 추가

--- src/main/java/com/example/app_230822/aticle/repository/ArticleRepository.java
+++ src/main/java/com/example/app_230822/aticle/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package com.example.app_230822.aticle.repository;
 
 import com.example.app_230822.aticle.dto.Article;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +19,26 @@ public interface ArticleRepository {
             </script>            
             """)
     List<Article> getArticles();
+
+    @Insert("""
+            <script>
+            INSERT INTO article
+            SET createDate = NOW(),
+            modifyDate = NOW(),
+            subject = #{subject},
+            content = #{content}
+            </script>
+            """)
+    void write(@Param("subject") String subject, @Param("content") String content); // 변경
+    // void write(String subject, String content); // 기존
+
+    @Select("""
+        <script>
+        SELECT *
+        FROM article
+        WHERE id = #{id}
+        </script>
+        """)
+    Article getArticleById(@Param("id") long id); // 변경
+    // Article getArticleById(long id); // 기존
 }
